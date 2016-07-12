f02c2378ab3dbd2e3c43146e116b47301eb00ae0
Bela Barabas <FSCYLEX\bela.barabas@ro-barabasb.cylex.local>
2016-07-12 15:12:25 +0300

    model corrected

--- core/ModelInterface.php
+++ core/ModelInterface.php
@@ -6,7 +6,7 @@ interface ModelInterface
 {
     public function connection(array $connectInfo);
     
-    public function getRecords(string $select);
+    public function getRecords(string $table, array $records = []);
     
     public function saveRecords(string $table, array $cols, array $records) : bool;
 }
--- core/Utils/Model.php
+++ core/Utils/Model.php
@@ -7,15 +7,31 @@ use Opis\Database\{Database, Connection};
 
 class Model implements ModelInterface {
     private $connection = null;
+    private $db = null;
+
     public function connection(array $connectInfo){
         if($this->connection == null)
             $this->connection = new Connection($connectInfo['host'], $connectInfo['user'], $connectInfo['password']);
+            
+        $this->db = new Database($this->connection);
         
-        return $this->connection;
+        return $this->db;
     }
     
-    public function getRecords(string $select) {
+    public function getRecords(string $table, array $records = []) {
         
+        if(empty($records)) {
+            
+        $result = $this->db->from($table)
+             ->select()
+             ->all();
+        }
+        else {
+            $result = $this->db->from($table)
+             ->select($records)
+             ->all();
+        }
+
     }
     
     public function saveRecords(string $table, array $cols, array $records) : bool {
