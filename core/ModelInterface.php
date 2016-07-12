e54127190225a6aee0d0df18b90560582246f74b
Bela Barabas <FSCYLEX\bela.barabas@ro-barabasb.cylex.local>
2016-07-12 16:13:41 +0300

    model corrected

--- core/ModelInterface.php
+++ core/ModelInterface.php
@@ -6,7 +6,7 @@ interface ModelInterface
 {
     public function connection(array $connectInfo);
     
-    public function getRecords(string $table, array $records = []);
+    public function getRecords(string $table, array $records = [], int $limit);
     
     public function saveRecords(string $table, array $cols, array $records) : bool;
 }
--- core/Utils/Model.php
+++ core/Utils/Model.php
@@ -11,27 +11,46 @@ class Model implements ModelInterface {
 
     public function connection(array $connectInfo){
         if($this->connection == null)
-            $this->connection = new Connection($connectInfo['host'], $connectInfo['user'], $connectInfo['password']);
+            $this->connection = new Connection('mysql:host='. $connectInfo['host'].';dbname='. $connectInfo['db'], $connectInfo['user'], $connectInfo['password']);
             
         $this->db = new Database($this->connection);
         
         return $this->db;
     }
     
-    public function getRecords(string $table, array $records = []) {
+    public function getRecords(string $table, array $records = [], int $limit = null) {
         
         if(empty($records)) {
+        
+            if($limit) {
+                $result = $this->db->from($table)
+                 ->limit($limit)
+                 ->select()
+                 ->all();
+            }
+            else {
+                $result = $this->db->from($table)
+                 ->where()
+                 ->select()
+                 ->all();    
+            }
             
-        $result = $this->db->from($table)
-             ->select()
-             ->all();
         }
         else {
-            $result = $this->db->from($table)
-             ->select($records)
-             ->all();
+            if($limit) {
+                $result = $this->db->from($table)
+                 ->limit($limit)
+                 ->select($records)
+                 ->all();
+            }
+            else {
+                $result = $this->db->from($table)
+                 ->where()
+                 ->select($records)
+                 ->all();    
+            }
         }
-
+        return $result;
     }
     
     public function saveRecords(string $table, array $cols, array $records) : bool {
