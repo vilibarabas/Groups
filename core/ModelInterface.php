a88e78332ad898874b6b34d25ad4c29c4917f367
Bela Barabas <FSCYLEX\bela.barabas@ro-barabasb.cylex.local>
2016-07-12 11:22:50 +0300

    git add groups project

---
+++ core/ModelInterface.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace Groups;
+
+interface ModelInterface
+{
+    public function connection(string $host, string $database, string $user, string $password);
+    
+    public function getRecords(string $select);
+    
+    public function saveRecords(string $table, array $cols, array $records) : boolean;
+}
---
+++ core/Utils/Model.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Groups\Utils;
+
+class Model implements ModelInterface {
+    
+    public function connection(string $host, string $database, string $user, string $password){
+        
+    }
+    
+    public function getRecords(string $select) {
+        
+    }
+    
+    public function saveRecords(string $table, array $cols, array $records) : boolean {
+        
+    }
+}
